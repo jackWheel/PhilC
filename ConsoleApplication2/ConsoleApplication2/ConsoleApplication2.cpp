9a0f42f79a460d3a52a46bc121bfa2658bd620c4
jackWheel <81240293+jackWheel@users.noreply.github.com>
2021-10-06 00:59:58 +0500

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,48 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");  //кириллица в консоле
+
+    cout << "Введите длину первого отрезка A: ";
+    int a;
+    cin >> a;
+
+    cout << "Введите длину второго отрезка B: ";
+    int b;
+    cin >> b;
+
+    int min, max;
+    int count = 0;
+    bool isALarger;
+
+    if (a == b) {              
+        cout << "Отрезки равны.";
+    }
+    else {
+        if (a > b) {                                                                                                                                                                                                                                                                                                  
+            min = b;
+            max = a;
+            isALarger = true;
+        }
+        else {
+            min = a;
+            max = b;
+            isALarger = false;
+        }
+        while (max >= min) {
+            max -= min;
+            count += 1;
+        }
+        if (isALarger) {
+            cout << "Отрезок B может поместиться в отрезке A " << count << " раз(а)." << endl;
+        }
+        else {
+            cout << "Отрезок A может поместиться в отрезке B " << count << " раз(а)." << endl;
+        }
+    }
+}
